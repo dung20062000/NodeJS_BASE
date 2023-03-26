063fec391c67f96ffae16223220e4a53e05e5a2e
Tien Dung <dungbuivm@gmail.com>
2023-03-26 09:02:34 +0700

    #4 DELETE, CREATE user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -15,7 +15,6 @@ let getDetailPage = async (req, res) => {
 
 //post user
 let createNewUser = async (req, res) => {
-  console.log('check request:', req.body);
   let {firstName, lastName, email, address} = req.body;
   //tương tự như là : let firstName = req.body.firstName
   await pool.execute('INSERT INTO users(firstName, lastName, email, address) VALUES (?, ?, ?, ? )', 
@@ -23,9 +22,36 @@ let createNewUser = async (req, res) => {
   
   return res.redirect('/')
 }
+//delete user
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute('DELETE FROM users WHERE id = ?', [userId] )
+  return res.redirect('/')
+}
+
+//edit user
+let editUser =  async (req, res) => {
+
+  let id = req.params.id;
+  let [user] = await pool.execute('SELECT * FROM users WHERE id = ?', [id]) // [user] is detructuring user
+  // console.log(user)
+  return res.render('update.ejs', {dataUser: user[0]}) // x <- y 
+}
+
+//update user
+  let updateUser = async  (req, res) => {
+    let {firstName, lastName, email, address, id} = req.body;
+    await pool.execute('UPDATE users set firstName =?, lastName=?, email=?, address=? WHERE id = ?', [firstName, lastName, email, address, id] )
+    return res.redirect('/')
+  }
+
 
 module.exports = {
     getHomePage,
     getDetailPage,
     createNewUser,
+    deleteUser,
+    editUser,
+    updateUser,
+
 }
